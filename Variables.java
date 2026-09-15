3daa0be68aa9852729a8d1883f27299ebd844eea
gayatrimarathe855-ux <gayatrimarathe855@gmail.com>
2026-09-15 22:49:59 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,14 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        String name = "Gayatri";
+        int age = 18;
+        double percentage = 85.5;
+        boolean student = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Student: " + student);
+    }
+}
