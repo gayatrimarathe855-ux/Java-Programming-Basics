6b92d558ead0b723374e33a798d27ecd59ea3390
gayatrimarathe855-ux <gayatrimarathe855@gmail.com>
2026-09-15 23:05:13 +0530

    Add files via upload

---
+++ public class Operators.java
@@ -0,0 +1,16 @@
+public class operators {
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+    }
+}
